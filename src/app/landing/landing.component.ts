2be0750d3fd1ec2d71984af01ee858af32fd2d32
mrinalnavin <mrinal.navin@gmail.com>
2019-09-24 17:57:14 +0530

    4 files modified

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -13,10 +13,18 @@ import { Router ,ActivatedRoute } from '@angular/router';
 export class LandingComponent implements OnInit {
  
   show:string;
+  donate:boolean=false;
+  request:boolean=false;
   constructor(private route: ActivatedRoute,
         private router: Router) {}
 ngOnInit(){
   this.show = this.route.snapshot.paramMap.get('id');
+  if(this.show==='donate'){
+    this.donate = true;
+  }
+  else if(this.show==='request'){
+    this.request=true;
+  }
   
 }
 
